3f767d35e4e1638445e06feff02d75b58a19a12c
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-08-02 22:08:51 +0100

    lineheight config tweaked, pic sizing figured out

--- app/page.js
+++ app/page.js
@@ -1,25 +1,26 @@
 import Image from 'next/image'
 import hero from './assets/heropic.png'
+import {headerFont, subheaderFont} from './utils/fonts.js'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row flex-wrap items-center justify-between p-24">
-      <div className="flex flex-col name-box">
-        <div className="flex header name1 text-9xl text-left">
-          <p>LIAM</p>
+    <main className="flex min-h-screen flex-row flex-wrap items-center justify-between px-24 ">
+      <div className="flex flex-col name-box md:text-center">
+        <div className="flex header name1 text-9xl">
+          <p className={headerFont.className}>LIAM</p>
         </div>
         <div className="flex header name2 text-9xl">
-          <p>SALMON</p>
+          <p className={headerFont.className}>SALMON</p>
         </div>
-        <div className="flex subheader name3 text-6xl">
-          <p>Full Stack Developer</p>
+        <div className="flex subheader name3 text-6xl text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">
+          <p className={subheaderFont.className}>Full Stack Div</p>
         </div>
       </div>
       <div className="flex flex-col imgbox">
         <Image src={hero} 
-        alt="hero" 
-        className="hero h-auto w-auto drop-shadow-2xl blur-none shadow-solid-primary"
-        layout='responsive'
+        alt="Picture of the author over orange ink splatter" 
+        className="hero"
+        style={{width: 475}}
         />
       </div>
     </main>
