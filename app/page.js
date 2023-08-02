eff9a98090ef511d84d53c1ac809d14530c101c1
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-08-02 18:49:06 +0100

    hero img added, styling todo

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Image from 'next/image'
+import hero from './assets/heropic.png'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-row flex-wrap items-center justify-between p-24">
-      
       <div className="flex flex-col name-box">
         <div className="flex header name1 text-9xl text-left">
           <p>LIAM</p>
@@ -9,12 +11,16 @@ export default function Home() {
         <div className="flex header name2 text-9xl">
           <p>SALMON</p>
         </div>
-        <div className="flex subheader name3 text-7xl">
+        <div className="flex subheader name3 text-6xl">
           <p>Full Stack Developer</p>
         </div>
       </div>
       <div className="flex flex-col imgbox">
-
+        <Image src={hero} 
+        alt="hero" 
+        className="hero h-auto w-auto drop-shadow-2xl blur-none shadow-solid-primary"
+        layout='responsive'
+        />
       </div>
     </main>
   )
