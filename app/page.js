25c8e8d1b50a82b2e2aab9e935640c49fe8b983b
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-08-02 15:24:35 +0100

    initializing files

---
+++ app/page.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-row flex-wrap items-center justify-between p-24">
+      
+      <div className="flex flex-col name-box">
+        <div className="flex header name1 text-9xl text-left">
+          <p>LIAM</p>
+        </div>
+        <div className="flex header name2 text-9xl">
+          <p>SALMON</p>
+        </div>
+        <div className="flex subheader name3 text-7xl">
+          <p>Full Stack Developer</p>
+        </div>
+      </div>
+      <div className="flex flex-col imgbox">
+
+      </div>
+    </main>
+  )
+}
---
+++ app/utils/fonts.js
@@ -0,0 +1,11 @@
+import { Jost, Jomhuria } from 'next/font/google'
+
+export const subheaderFont = Jost({
+    weight: ["500", "600", "700"],
+  subsets: ["latin"]
+})
+
+export const headerFont = Jomhuria({
+    weight: ["400"],
+    subsets: ["latin"]
+})
