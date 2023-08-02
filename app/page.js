f654ce4af8cc9c02dcb9be3df7899198fe0ffc07
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-08-03 00:03:08 +0100

    box links created, styling tweaks, coffee img modified

--- app/page.js
+++ app/page.js
@@ -1,28 +1,50 @@
 import Image from 'next/image'
 import hero from './assets/heropic.png'
-import {headerFont, subheaderFont} from './utils/fonts.js'
+import coffee from './assets/coffee.png'
+import { headerFont, subheaderFont } from './utils/fonts.js'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row flex-wrap items-center justify-between px-24 ">
-      <div className="flex flex-col name-box md:text-center">
-        <div className="flex header name1 text-9xl">
-          <p className={headerFont.className}>LIAM</p>
+    <main className="flex min-h-screen flex-col flex-wrap items-center justify-between">
+      <section className="maintop flex flex-row flex-wrap justify-between w-full  px-28 mx-auto my-[3rem]">
+        <div className="flex flex-col name-box md:text-center">
+          <div className="flex header name1 text-9xl cursor-default">
+            <p className={headerFont.className}>LIAM</p>
+          </div>
+          <div className="flex header name2 text-9xl cursor-default">
+            <p className={headerFont.className}>SALMON</p>
+          </div>
+          <div className="flex subheader name3 text-6xl text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] cursor-default">
+            <p className={subheaderFont.className}>Full Stack Div</p>
+          </div>
         </div>
-        <div className="flex header name2 text-9xl">
-          <p className={headerFont.className}>SALMON</p>
+        <div className="flex flex-col imgbox relative mr-[2rem]">
+          <Image src={hero}
+            alt="Picture of the author over orange ink splatter"
+            className="hero rotate-[-5deg] aspectratio-auto"
+            style={{ width: "28rem", minWidth: "20rem" }}
+          />
+          <Image src={coffee}
+            alt="Picture of coffee cup"
+            className="coffee absolute -right-2 top-4 rotate-[15deg]"
+            style={{ width: "8rem" }}
+          />
         </div>
-        <div className="flex subheader name3 text-6xl text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">
-          <p className={subheaderFont.className}>Full Stack Div</p>
+      </section>
+      <section className="mainbottom flex flex-row w-full ">
+        <div className="flex flex-row flex-wrap text-box justify-between m-auto">
+          <div className="bottombox box1 h-[7rem] m-auto mx-9 bg-white w-[20rem] border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] mb-[2rem] p-[0.2rem]">
+            <span className={subheaderFont.className}>Projects</span>
+          </div>
+          <div className="bottombox box2 h-[7rem] m-auto  mx-9 bg-black text-white w-[20rem] border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] mb-[2rem] p-[0.2rem]">
+            <span className={subheaderFont.className}>About</span>
+          </div>
+          <div className="bottombox box3 h-[7rem] m-auto  mx-9 bg-white w-[20rem] border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] mb-[2rem] p-[0.2rem]">
+            <span className={subheaderFont.className}>Contact</span>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col imgbox">
-        <Image src={hero} 
-        alt="Picture of the author over orange ink splatter" 
-        className="hero"
-        style={{width: 475}}
-        />
-      </div>
+      </section>
+
     </main>
   )
 }
