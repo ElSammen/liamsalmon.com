5881b2c9866813bc09645aff93bc47003dcc6e49
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-08-05 00:49:11 +0100

    dummy cards added to projects page

--- app/pages/projects/page.js
+++ app/pages/projects/page.js
@@ -11,6 +11,22 @@ export default function Projects() {
                 </Link>
                 hehe you shouldtn be here xd
                 projecto
+                <div className="projectbox flex sm:flex-row flex-wrap justify-between w-full sm:mx-auto sm:my-[3rem] text-center md: text-left">
+                    <div className="flex flex-col bg-fuchsia-500 border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] w-[20rem] h-[4rem] text-center">
+                        <p className="border-b-4 border-black w-[50%] mx-auto">test</p>
+                    </div>
+                        
+                    <div className="flex flex-col projcard bg-violet-500 border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] w-[20rem] h-[4rem] text-center">
+                    
+                    </div>
+
+                    <div className="flex flex-col bg-fuchsia-500 border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] w-[20rem] h-[4rem] text-center">
+                    
+                    </div>
+
+                    <div className="flex flex-col bg-violet-500 border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] w-[20rem] h-[4rem] text-center">
+                    </div>
+                </div>
             </section>
         </main>
 
