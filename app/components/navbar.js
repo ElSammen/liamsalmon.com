10c74aa22969a5695885bf7889a3b0aa2c9251c5
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-08-23 14:59:12 +0100

    testing mobile responsiveness, modal still TODO

---
+++ app/components/navbar.js
@@ -0,0 +1,126 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState, useCallback, useEffect } from 'react'
+import { subheaderFont, headerFont } from '../utils/fonts'
+import borgor from '../assets/borgor.png'
+
+function navbar() {
+
+
+    const useMediaQuery = (width) => {
+        const [targetReached, setTargetReached] = useState(false);
+
+        const updateTarget = useCallback((e) => {
+            if (e.matches) {
+                setTargetReached(true);
+            } else {
+                setTargetReached(false);
+            }
+        }, []);
+
+        useEffect(() => {
+            const media = window.matchMedia(`(max-width: ${width}px)`);
+            media.addListener(updateTarget);
+
+            // Check on mount (callback is not called until a change occurs)
+            if (media.matches) {
+                setTargetReached(true);
+            }
+
+            return () => media.removeListener(updateTarget);
+        }, []);
+
+        return targetReached;
+    };
+
+    const isBreakpoint = useMediaQuery(768)
+
+
+    /* handleMenu will be used to toggle the menu of the options */
+
+    const [menu, setMenu] = useState(false)
+
+    const handleMenu = () => {
+       setMenu(!menu)
+    }
+    
+
+
+    return (
+        <div className='w-screen absolute top-0 bg-sky-600 border-b-2 h-10 border-sky-700 leading-0'>
+            <div className='flex flex-row justify-between'>
+                <Link href='/'>
+                    <div className={headerFont.className}>
+                        <p className='ml-2 text-[3rem] w-[10rem] text-white h-10  headerText
+                    decoration-2 underline-offset-2 hover:underline hover:cursor-pointer'
+                        >LIAM SALMON</p>
+                    </div>
+                </Link>
+
+                {!isBreakpoint ? <div className='justify-self-end flex flex-row gap-3'>
+                    <Link href='/pages/projects' >
+                        <div className={headerFont.className}>
+                            <p className='mr-2  text-[3rem] text-white h-10  headerText
+                    decoration-2 underline-offset-2 hover:text-green-500 hover:underline hover:cursor-pointer'
+                            >PROJECTS</p>
+                        </div>
+                    </Link>
+                    <Link href='/pages/about' >
+                        <div className={headerFont.className}>
+                            <p className='mr-2  text-[3rem] text-white h-10  headerText
+                    decoration-2 underline-offset-2 hover:text-amber-400 hover:underline hover:cursor-pointer'
+                            >ABOUT</p>
+                        </div>
+                    </Link>
+                    <Link href='/pages/contact' >
+                        <div className={headerFont.className}>
+                            <p className='mr-2  text-[3rem] text-white h-10  headerText
+                    decoration-2 underline-offset-2 hover:text-violet-600 hover:underline hover:cursor-pointer'
+                            >CONTACT</p>
+                        </div>
+                    </Link>
+                </div> :
+                    <div className='justify-self-end flex flex-row my-auto mr-2'
+                        onClick={() => handleMenu()}
+                    >
+                        <Image src={borgor} alt='burger menu' className='w-8 h-8' />
+                        { menu ? 
+                            <div className='absolute top-9 right-1 bg-sky-600 p-1 border-b-2  border-black shadow-[4px_4px_0px_rgba(0,0,0,1)] leading-0'>
+                                <Link href='/pages/projects' >
+                                    <div className={headerFont.className}>
+                                        <p className='mr-2  text-[5rem] text-white  headerText
+                    decoration-2 underline-offset-2 blrd'
+                                        >PROJECTS</p>
+                                        </div>
+                                </Link>
+                                <Link href='/pages/about' >
+                                    <div className={headerFont.className}>
+                                        <p className='mr-2  text-[5rem] text-white  headerText
+                    decoration-2 underline-offset-2 '
+                                        >ABOUT</p>
+                                        </div>
+                                        </Link>
+                                <Link href='/pages/contact' >
+                                    <div className={headerFont.className}>
+                                        <p className='mr-2  text-[5rem] text-white   headerText
+                    decoration-2 underline-offset-2'
+                                        >CONTACT</p>
+                                        </div>
+                                        </Link>
+                            </div> : <></>
+                        }
+
+                            
+                    </div>
+                }
+            </div>
+
+        </div>
+
+    )
+}
+
+export default navbar
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from './components/navbar.js'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
-        
+        <Navbar />
       </body>
     </html>
   )
