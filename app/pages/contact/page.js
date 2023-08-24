a177da36db952e704a98c299ff8d904bd9daa02d
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-08-24 23:48:37 +0100

    why did these not get pushed oopsy

--- app/pages/contact/page.js
+++ app/pages/contact/page.js
@@ -1,11 +1,19 @@
-import Link from "next/link"
+import ContactForm from "./contactme.js"
 
 export default function Contact() {
+
     return (
         <main className="bg-sky-500 flex min-h-screen min-w-screen mt-14 md:mt-0 flex-col flex-wrap items-center justify-between">
             <section className="maintop flex sm:flex-row flex-wrap justify-between w-full  px-28 sm:mx-auto sm:my-[3rem] text-center md:text-left">
-                <div>
-                    <h1 className="text-5xl font-bold text-white">Get in touch!</h1>
+                <div className="left ">
+                    <h1 className="text-5xl font-bold">Get in touch!</h1>
+
+                    <ContactForm />
+                    
+                </div>
+
+                <div className="">
+                    test post pls ignore
                 </div>
             </section>
         </main>
