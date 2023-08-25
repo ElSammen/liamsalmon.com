bd77a5b4a3a9069950b070441c8c498e93b142ce
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-08-25 18:01:48 +0100

    target blank for links

--- app/pages/contact/page.js
+++ app/pages/contact/page.js
@@ -26,16 +26,16 @@ export default function Contact() {
 
 
                         <div className="flex flex-row justify-center justify-self-center align-items-center my-[30%]">
-                            <Link href="https://www.linkedin.com/in/liam-salmon-9053b8265/">
+                            <Link href="https://www.linkedin.com/in/liam-salmon-9053b8265/" target="_blank">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="mt-[-0.1rem]" x="0px" y="0px" width="56" viewBox="0 0 50 50"
                                     style={{ fill: '#1A1A1A' }}>
                                     <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
                                 </svg>
                             </Link>
-                            <Link href="https://github.com/ElSammen" >
+                            <Link href="https://github.com/ElSammen" target="_blank">
                                 <Image src={github} alt="GitHub" width={50} height={50} />
                             </Link>
-                            <Link href="https://docs.google.com/document/d/e/2PACX-1vRTNDBfiiJFRHqduz-SzKs7BaxUb5X1gv24ZgN_ofI9djbE2OUASMpyfsmPkID6oORl5VbdQconx8iq/pub" >
+                            <Link href="https://docs.google.com/document/d/e/2PACX-1vRTNDBfiiJFRHqduz-SzKs7BaxUb5X1gv24ZgN_ofI9djbE2OUASMpyfsmPkID6oORl5VbdQconx8iq/pub" target="_blank">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="mt-[-0.3rem]" height="60" viewBox="0 0 24 24" width="60"><path d="M0 0h24v24H0z" fill="none" /><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z" /></svg>
                             </Link>
 
